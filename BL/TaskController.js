7d061220864c2ec1faae5542457d66240d124fb5
Sara Mazal <32996161+saramazal@users.noreply.github.com>
2021-12-29 13:13:13 +0200

    commit

---
+++ BL/TaskController.js
@@ -0,0 +1,9 @@
+const uniqid = require('uniqid')
+const DAL = require('../DAL/index')
+
+function create(data) {
+    data.id = uniqid()
+    return DAL.create(data)
+}
+
+module.exports = { ...DAL, create }
